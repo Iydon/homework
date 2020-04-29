f3721c51fe735677c37bc519303eb2e0a6fb1b0a
iydon <11711217@mail.sustech.edu.cn>
2020-04-29 16:34:38 +0800

    MA110: project 1 finished

--- MA110/project/main.m
+++ MA110/project/main.m
@@ -8,7 +8,7 @@ verbose = false;
 
 number = length({dir(dir_name).name}) - 2;  % . and ..
 [File, Size] = deal(string(NaN(number, 1)));
-Time = zeros(number, 1);
+[Time, Correct] = deal(zeros(number, 1));
 for ith = 1: number
     % Paint, cKey, rKey
     file_name = fullfile(dir_name, [num2str(ith),'.mat']);
@@ -19,14 +19,15 @@ for ith = 1: number
     File(ith) = string(file_name);
     Size(ith) = sprintf('%dx%d', size(Paint));
     Time(ith) = time;
+    Correct(ith) = ~sum(sum(result ~= Paint));
     if verbose
-        fprintf('Data path (%dx%d): %s\n', size(Paint), file_name);
-        fprintf('Is corrected? %d\n', ~sum(sum(result ~= Paint)));
+        fprintf('Data path (%s): %s\n', Size(ith), file_name);
+        fprintf('Is corrected? %d\n', Correct(ith));
         fprintf('Elapsed time is %.3f seconds.\n', time);
         fprintf('\n');
     end
 end
-Records = table(File, Size, Time);
+Records = table(File, Size, Time, Correct);
 disp(Records);
 
 
